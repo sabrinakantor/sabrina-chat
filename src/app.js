089cb32256c6d74373eb33387e01f45c0e13e125
sabrinakantor <sabrinakantor@gmail.com>
2024-07-14 18:50:15 -0400

    adds gemini api

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+require('dotenv').config();
+
+const {
+  GoogleGenerativeAI,
+} = require("@google/generative-ai");
 
 require('dotenv').config();
 
@@ -15,11 +20,36 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({
-    message: '🦄🌈✨👋🌎🌍🌏✨🌈🦄',
+const chatWithGemini = async (req, res) => { 
+  const apiKey = process.env.API_KEY;
+  console.log(apiKey);
+  const genAI = new GoogleGenerativeAI(apiKey);
+
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
   });
-});
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+
+  const chatSession = model.startChat({
+    generationConfig,
+ // safetySettings: Adjust safety settings
+ // See https://ai.google.dev/gemini-api/docs/safety-settings
+    history: [
+    ],
+  });
+
+  const result = await chatSession.sendMessage("hi there. whats up");  
+  res.send(result)
+}
+
+app.get('/', chatWithGemini);
 
 app.use('/api/v1', api);
 
